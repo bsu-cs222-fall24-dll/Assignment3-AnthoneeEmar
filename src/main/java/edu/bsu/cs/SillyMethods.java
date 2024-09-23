8c1e9f99cd8a244eefb1e8c295a7bead411048f3
antho <anthoneelemar@gmail.com>
2024-09-23 02:13:51 -0400

    Initial Commit

---
+++ src/main/java/edu/bsu/cs/SillyMethods.java
@@ -0,0 +1,15 @@
+package edu.bsu.cs;
+
+public class SillyMethods {
+    public boolean didGuess42(int year) {
+    return true;
+    }
+
+    public String countTo10(int year) {
+        return "0 1 2 3 4 5 6 7 8 9 10";
+    }
+
+    public String countTo(int year) {
+    return "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15";
+    }
+}
